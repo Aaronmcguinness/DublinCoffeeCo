dfd9ef295fc788adaa9e4862461900d9d58eba67
Webdesign16 <p.jmoran@icloud.com>
2016-11-17 00:14:49 +0000

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 200px */
+function openNav() {
+    document.getElementById("myleftnav").style.width = "200px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("myleftnav").style.width = "0";
+}
